0240e87d5389a25444d60766ae2d0bad40c405a6
zhichaoGuo <cn.guozhichao@gmail.com>
2022-12-12 10:12:39 +0800

    add README

---
+++ README.md
@@ -0,0 +1,7 @@
+- 用于内部文件服务器上传
+- 使用方法：在目录中使用
+- python3.9 ./server.py --bind 127.0.0.1 --port 8080 --folder /var/www/
+- 在http.server项目基础上添加分享连接及删除文件的功能
+- 添加查看log的界面
+- 添加登录及注册的功能
+- 数据库使用mysql
--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class Config:
     log_path = "./log/flask.log"
     log_format = "[%(asctime)s][%(module)s:%(lineno)d][%(levelname)s][%(thread)d] - %(message)s"
-    secret_key = 'Htek20180905'
+    secret_key = 'Http20221212'
     sql_root_name = 'root'
-    sql_root_pass = 'HtekRPS2017'
+    sql_root_pass = 'Http20121212'
     sql_auth_db = 'http'
     sql_port = '3306'
     sql_url = f'mysql+pymysql://{sql_root_name}:{sql_root_pass}@127.0.0.1:{sql_port}/{sql_auth_db}'
-    register_key = 'Htek20180905'
+    register_key = 'Http20121212'
